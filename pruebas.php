3cef4357f1fc5455657c4bc9245a01c1879d28ae
Jose Lameiras <joselameiras@gmail.com>
2023-10-28 08:17:08 -0600

    pruebas

--- pruebas.php
+++ pruebas.php
@@ -1,37 +1,14 @@
-<?php
-//SplStack Mode is LIFO (Last In First Out)
-  
-$q = new SplQueue();
-$q->push(1);
-$q->push(2);
-$q->push(3);
-$q->pop();
-$q->push(4);
-$q->push(5);
-$q->push(6);
-$q->push(7);
-
-$q->rewind();
-while($q->valid()){
-    echo $q->current(),"\n";
-    $q->next();
-}
-?>
-
-
  <script>
-        function convierteletra(letra) {
-            return letra.charCodeAt(0);
-        }
-        var nombrecompleto = ['Jose ', 'Lameiras ', 'Pleites '];
+      function myFunc(elobjeto) {
+  elobjeto.marca = "Toyota";
+}
+var mycar = {
+  marca: "Honda",
+  modelo: "Accord",
+  año: 1998,
+};
 
-        for (i = 0; i < nombrecompleto.length; i++) {
-            for (e = 0; e < nombrecompleto[i].length; e++) {
-                console.log(convierteletra(nombrecompleto[i][e]));
-            }
-        }
-
-
-
-
+window.alert(mycar.marca);
+myFunc(mycar);
+window.alert(mycar.marca);
     </script>
